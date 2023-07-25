ba775d8abb5c27c98960434378f7b66f4a854699
santiago <bassilsantiago@gmail.com>
2023-07-25 14:45:25 -0500

    Feat: Add route to follow users

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const userController = require("../Controllers/userController")
+const checkAuh = require("../Controllers/checkAuth")
 
 //GET Requests
-router.get("/:username", userController.UserProfile)
+router.get("/:username", checkAuh, userController.UserProfile)
 
 //POST Requests
 router.post("/register", userController.Register);
 router.post("/login", userController.Login);
+
+router.post("/:username/follow", checkAuh, userController.FollowUser)
 
 module.exports = router;
