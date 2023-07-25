d29b120a2fcd452d8886765a9f1b5050778beecd
santiago <bassilsantiago@gmail.com>
2023-07-24 21:55:01 -0500

    Configure firebase

---
+++ FirebaseApp.js
@@ -0,0 +1,19 @@
+const {initializeApp} = require("firebase/app")
+const { getFirestore } = require("firebase/firestore")
+require("dotenv").config
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCETUz72R0uSbIB-_0b48oUoGu-8Lj2qO8",
+    authDomain: "intagram-clone-8d19d.firebaseapp.com",
+    projectId: "intagram-clone-8d19d",
+    storageBucket: "intagram-clone-8d19d.appspot.com",
+    messagingSenderId: "708060846981",
+    appId: "1:708060846981:web:4bad62ef7a4709973302ec"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+module.exports = {
+    db
+}
