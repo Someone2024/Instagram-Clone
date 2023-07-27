80e4b578fa90aedd918ac6b499da954e6d4782b5
Someone <bassilsantiago@gmail.com>
2023-07-27 14:19:18 -0500

    Feat: Return user data based on privacy settings

--- Controllers/UserActions/GetUserProfile.js
+++ Controllers/UserActions/GetUserProfile.js
@@ -2,27 +2,56 @@ const { getDocs, query, where, limit } = require("firebase/firestore");
 const { UsersRef } = require("../../FirebaseApp");
 
 exports.GetUserProfile = async (req, res) => {
-  const { username } = req.params;
-  const userQuery = query(
+  const userToGet = req.params.username;
+  const currentUser = req.username
+
+  const userToGetQuery = query(
     UsersRef,
-    where("username", "==", username),
+    where("username", "==", userToGet),
+    limit(1)
+  );
+
+  const currentUserQuery = query(
+    UsersRef,
+    where("username", "==", currentUser),
     limit(1)
   );
   try {
-    const user = await getDocs(userQuery);
-    const userData = user.docs[0].data();
+    const userToGet = await getDocs(userToGetQuery);
+    const userToGetData = userToGet.docs[0].data();
 
-    if (userData.privacySettings.private) {
+    const currentUser = await getDocs(currentUserQuery);
+    const currentUserData = currentUser.docs[0].data();
+
+    const verifyUserToGetRelationShip = userToGetData.relationShips.some(
+      (obj) => {
+        if (
+          obj["username"] === currentUserData.username &&
+          obj["relationShip_type"] === "blocked"
+        )
+          return true;
+        else false;
+      }
+    );
+
+    if (userToGetData.privacySettings.private_account) {
       res.json({
+        userName: userToGetData.username,
         error: "This Account is private",
       });
+    } else if(verifyUserToGetRelationShip){
+      res.json({
+        error: "This user either has blocked you or you have bloked them",
+      });
     }
-    res.json({
-      userName: userData.username,
-      userBio: userData.bio,
-      number_of_posts: userData.number_of_posts,
-      number_of_followers: userData.number_of_followers,
-      number_of_following: userData.number_of_following,
-    });
+    else {
+      res.json({
+        userName: userToGetData.username,
+        userBio: userToGetData.bio,
+        number_of_posts: userToGetData.number_of_posts,
+        number_of_followers: userToGetData.number_of_followers,
+        number_of_following: userToGetData.number_of_following,
+      });
+    }
   } catch (e) {}
 };
