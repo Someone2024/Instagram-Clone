8a354a67cfb773af9df3ba8f8cf2fc7433ea4ad7
santiago <bassilsantiago@gmail.com>
2023-07-26 19:03:46 -0500

    Feat: Verify if user is already followed and follows or unfollows accordingly

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -106,40 +106,92 @@ exports.FollowUser = async (req, res) => {
     limit(1)
   );
   try {
-   const userToFollowSnapshot = await getDocs(userToFollowQuery);
-   const currentUserSnapshot = await getDocs(currentUserQuery);
+    const userToFollowSnapshot = await getDocs(userToFollowQuery);
+    const currentUserSnapshot = await getDocs(currentUserQuery);
 
-  const userToFollowData = userToFollowSnapshot.docs[0].data();
-  const currentUserData = currentUserSnapshot.docs[0].data();
+    const userToFollowData = userToFollowSnapshot.docs[0].data();
+    const currentUserData = currentUserSnapshot.docs[0].data();
 
-    await updateDoc(userToFollowSnapshot.docs[0].ref, {
-      number_of_followers: + 1,
-      relationShips: [
-        {
-          username: currentUserData.username,
-          relationShip_type: "follower",
-        },
-      ],
-    });
+    const verifyUserToFollowRelationShip = userToFollowData.relationShips.some(
+      (obj) => obj["username"] === currentUserData.username
+    );
 
-    await updateDoc(currentUserSnapshot.docs[0].ref, {
-      number_of_following: + 1,
-      relationShips: [
-        {
-          username: userToFollowData.username,
-          relationShip_type: "following",
-        },
-      ],
-    });
+    const updateBothUsers = async (
+      usertoupdate,
+      fieldtoupdate,
+      relationshiptoupdate,
+      usertoupdaterelationshipwith,
+      operationtoexecute
+    ) => {
+      await updateDoc(usertoupdate.docs[0].ref, {
+        fieldtoupdate: eval(`${operationtoexecute} 1`),
+        relationShips: [
+          {
+            username: usertoupdaterelationshipwith,
+            relationShip_type: relationshiptoupdate,
+          },
+        ],
+      });
+    };
 
-  res.json({
-    Message: "Success"
-  })
-  
+    const deleteRelationShip = async (
+      usertoupdate,
+      _fieldtoupdate,
+      operationtoexecute,
+      relationshipToDelete
+    ) => {
+      await updateDoc(usertoupdate.docs[0].ref, {
+        _fieldtoupdate: eval(`${operationtoexecute} 1`),
+        relationShips: usertoupdate.docs[0]
+          .data()
+          .relationShips.filter((relationship) => {
+            return relationship.username !== relationshipToDelete;
+          }),
+      });
+    };
+
+    if (!verifyUserToFollowRelationShip) {
+      await updateBothUsers(
+        userToFollowSnapshot,
+        "number_of_followers",
+        "follower",
+        currentUserData.username,
+        "+"
+      );
+
+      await updateBothUsers(
+        currentUserSnapshot,
+        " number_of_following",
+        "following",
+        userToFollowData.username,
+        "+"
+      );
+
+      res.json({
+        message: "user followed :)",
+      });
+    } else {
+      await deleteRelationShip(
+        userToFollowSnapshot,
+        "number_of_followers",
+        "-",
+        currentUserData.username
+      );
+
+      await deleteRelationShip(
+        currentUserSnapshot,
+        "number_of_following",
+        "-",
+        userToFollowData.username
+      );
+
+      res.json({
+        message: "already following this user",
+      });
+    }
   } catch (err) {
     res.status(500).json({
-      message: "Failed to log in.",
-      err,
+      message: "Failed to follow this user." + err,
     });
   }
 };
