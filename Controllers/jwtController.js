bc78051cf87b1d6d4019a972a9ca882e1fe8a32b
santiago <bassilsantiago@gmail.com>
2023-07-26 22:17:11 -0500

    Fix: increment the expiration date of the token

--- Controllers/jwtController.js
+++ Controllers/jwtController.js
@@ -6,7 +6,7 @@ dotenv.config();
 const JWT_SECRET = process.env.JWT_SECRET;
 
 const generateToken = (payload) => {
-  return jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "100h" });
 };
 
 const verifyToken = (token) => {
