e489c51eb9d26fd39a00d38ec5cd111b16e9467f
Someone <bassilsantiago@gmail.com>
2023-07-27 16:30:39 -0500

    Update: Create reference to firebase stoagre

--- FirebaseApp.js
+++ FirebaseApp.js
@@ -1,20 +1,24 @@
 const {initializeApp} = require("firebase/app")
 const { getFirestore } = require("firebase/firestore")
 const {collection} = require("firebase/firestore")
+const {getStorage, ref} = require("firebase/storage")
 require("dotenv").config
 
 const firebaseConfig = {
     apiKey: "AIzaSyCETUz72R0uSbIB-_0b48oUoGu-8Lj2qO8",
     authDomain: "intagram-clone-8d19d.firebaseapp.com",
     projectId: "intagram-clone-8d19d",
-    storageBucket: "intagram-clone-8d19d.appspot.com",
+    storageBucket: "gs://intagram-clone-8d19d.appspot.com/",
     messagingSenderId: "708060846981",
     appId: "1:708060846981:web:4bad62ef7a4709973302ec"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
+const storage = getStorage()
+
 const UsersRef = collection(db, "Users");
+const userProfileImages = ref(storage, "images")
 
 module.exports = {
     db,
