11aa6617ee00759fb944e0d5e6cd71304e90c583
santiago <bassilsantiago@gmail.com>
2023-07-26 19:38:37 -0500

    Refactor: import user actions

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -10,37 +10,12 @@ const { db } = require("../FirebaseApp");
 const { Register } = require("./UserActions/RegisterUser");
 const { Login } = require("./UserActions/LoginUser");
 const { FollowOrUnFollowUser } = require("./UserActions/FollowOrUnUser");
-
-const UsersRef = collection(db, "Users");
+const { GetUserProfile } = require("./UserActions/GetUserProfile");
 
 exports.Register = Register;
 
 exports.Login = Login;
 
-exports.UserProfile = async (req, res) => {
-  const { username } = req.params;
-  const userQuery = query(
-    UsersRef,
-    where("username", "==", username),
-    limit(1)
-  );
-  try {
-    const user = await getDocs(userQuery);
-    const userData = user.docs[0].data();
-
-    if (userData.privacySettings.private) {
-      res.json({
-        error: "This Account is private",
-      });
-    }
-    res.json({
-      userName: userData.username,
-      userBio: userData.bio,
-      number_of_posts: userData.number_of_posts,
-      number_of_followers: userData.number_of_followers,
-      number_of_following: userData.number_of_following,
-    });
-  } catch (e) {}
-};
+exports.GetUserProfile = GetUserProfile
 
 exports.FollowOrUnFollowUser = FollowOrUnFollowUser
