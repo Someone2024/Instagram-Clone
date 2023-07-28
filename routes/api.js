e42c704909d0c3012244d8b9b70daf6228e95fb0
santiago <bassilsantiago@gmail.com>
2023-07-28 08:18:27 -0500

    Feat: configure multer to handle file uploads

--- routes/api.js
+++ routes/api.js
@@ -1,21 +1,29 @@
-const express = require("express")
-const router = express.Router()
-const userController = require("../Controllers/userController")
-const checkAuh = require("../Controllers/checkAuth")
+const express = require("express");
+const router = express.Router();
+const userController = require("../Controllers/userController");
+const checkAuh = require("../Controllers/checkAuth");
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: "./uploads",
+  filename: function (req, file, cb) {
+    // Use the original file name
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
 
 //GET Requests
-router.get("/:username", checkAuh, userController.GetUserProfile)
+router.get("/:username", checkAuh, userController.GetUserProfile);
 
 //POST Requests
 router.post("/register", userController.Register);
 router.post("/login", userController.Login);
 
-router.post("/:username/follow", checkAuh, userController.FollowOrUnFollowUser)
-router.post("/:username/block", checkAuh, userController.BlockOrUnBlockUser)
+router.post("/:username/follow", checkAuh, userController.FollowOrUnFollowUser);
+router.post("/:username/block", checkAuh, userController.BlockOrUnBlockUser);
 
 //PUT requests
-router.put("/settings/profile", checkAuh, userController.UpdateUserProfile)
-router.put("/settings/privacy", checkAuh, userController.UpdateUserPrivacy)
-
+router.post("/settings/profile", checkAuh, upload.single("file"), userController.UpdateUserProfile);
+router.put("/settings/privacy", checkAuh, userController.UpdateUserPrivacy);
 
 module.exports = router;
