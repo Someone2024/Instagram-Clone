a02b9d9f489218525b312fdf002cc6ff1eb7464d
santiago <bassilsantiago@gmail.com>
2023-07-28 18:30:56 -0500

    Fix: add output path for multer files

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,13 @@ const userController = require("../Controllers/userController");
 const checkAuh = require("../Controllers/checkAuth");
 const multer = require("multer");
 const storage = multer.diskStorage({
-  destination: "./uploads",
-  filename: function (req, file, cb) {
-    // Use the original file name
-    cb(null, file.originalname);
-  },
-});
-const upload = multer({ storage: storage });
+    destination: 'uploads/',
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
 
 //GET Requests
 router.get("/:username", checkAuh, userController.GetUserProfile);
@@ -23,7 +23,7 @@ router.post("/:username/follow", checkAuh, userController.FollowOrUnFollowUser);
 router.post("/:username/block", checkAuh, userController.BlockOrUnBlockUser);
 
 //PUT requests
-router.post("/settings/profile", checkAuh, upload.single("file"), userController.UpdateUserProfile);
+router.put("/settings/profile/picture", upload.single("file"), checkAuh, userController.UpdateUserProfilePicture);
 router.put("/settings/privacy", checkAuh, userController.UpdateUserPrivacy);
 
 module.exports = router;
