7964c503c743057e7444111be8c47a3a9ec91799
santiago <bassilsantiago@gmail.com>
2023-07-28 21:49:51 -0500

    Feat: import upload controller

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,8 +1,9 @@
+const { GetUserProfile } = require("./UserActions/GET/GetUserProfile");
 const { Register } = require("./UserActions/POST/RegisterUser");
 const { Login } = require("./UserActions/POST/LoginUser");
 const { FollowOrUnFollowUser } = require("./UserActions/POST/FollowOrUnUser");
-const { GetUserProfile } = require("./UserActions/GET/GetUserProfile");
 const { BlockOrUnBlockUser } = require("./UserActions/POST/BlockOrUnUser");
+const { Upload } = require("./UserActions/POST/Upload");
 const { UpdateUserPrivacy } = require("./UserActions/PUT/UpdateUserPrivacy");
 const { UpdateUserProfilePicture } = require("./UserActions/PUT/UpdateUserProfilePicture");
 const { UpdateUserProfile } = require("./UserActions/PUT/UpdateUserProfile");
@@ -22,3 +23,5 @@ exports.UpdateUserPrivacy = UpdateUserPrivacy;
 exports.UpdateUserProfilePicture = UpdateUserProfilePicture;
 
 exports.UpdateUserProfile = UpdateUserProfile;
+
+exports.Upload = Upload;
