c54cf1c318c5ccd6f1fc8e7ed89c52b7520417f0
santiago <bassilsantiago@gmail.com>
2023-07-28 19:39:38 -0500

    Feat: recives files, checks for its type, and saves them in the user model

--- Controllers/UserActions/PUT/UpdateUserProfilePicture.js
+++ Controllers/UserActions/PUT/UpdateUserProfilePicture.js
@@ -1,25 +1,59 @@
-const { storage } = require("../../../FirebaseApp");
-const { ref, uploadBytes } = require("firebase/storage");
 const  fs  = require("fs/promises");
 const multer = require("multer");
 const upload = multer({ dest: "uploads/" });
 const handleFile = upload.single("file")
+const {
+  query,
+  where,
+  limit,
+  getDocs,
+  updateDoc,
+} = require("firebase/firestore");
+const { UsersRef } = require("../../../FirebaseApp");
 
 exports.UpdateUserProfilePicture = async (req, res) => {
-  //here is where we encode the image into a string
-  //then in routes that require the users profile pic
-  //we get the base64 string from the profile_picture in the user model
-  //decode it into an image format
-  //and then we send that in the response as json
   const fileData = req.file;
-  const encodedFile = (await fs.readFile(fileData.path)).toString("base64");
-  const BufferedFile = Buffer.from(encodedFile, "base64")
-  const imagesDir = await fs.mkdir("decoded/", {recursive: true})
-  const decodedFile = await fs.writeFile(`decoded/${fileData.originalname}`, BufferedFile)
+  const encodedFile = `data:image/png;base64,${(await fs.readFile(fileData.path)).toString("base64")}`;
+  const currentUser = req.username;
+  const currentUserQuery = query(
+    UsersRef,
+    where("username", "==", currentUser),
+    limit(1)
+  );
 
-  res.json({
-    decodedFile
-  })
+  const allowedMimeTypes = [
+    "image/jpeg",
+    "image/png",
+    "image/gif",
+    "image/bmp",
+    "image/tiff",
+    "image/webp",
+    "image/svg+xml",
+    "image/vnd.microsoft.icon",
+    "image/jfif",
+  ];
+  if (!allowedMimeTypes.includes(req.file.mimetype)) {
+    res.json({
+      message: "Error file format",
+      f: fileData
+    })
+    await fs.rm(`${fileData.path}`)
+  } 
 
-  // const deleteDir = await fs.rm(`decoded/${fileData.originalname}`)
+  try {
+    const currentUserSnapshot = await getDocs(currentUserQuery);
+
+    await updateDoc(currentUserSnapshot.docs[0].ref, {
+      profile_picture: encodedFile
+    });
+
+    res.json({
+      message: fileData
+    })
+    await fs.rm(`${fileData.path}`)
+  }catch(e) {
+    res.json({
+      message: "an error has ocurred" + e
+    })
+  }
 };
