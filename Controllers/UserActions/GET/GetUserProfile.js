23e658fb136481f0e8ce0c9561e3e9810dad815a
santiago <bassilsantiago@gmail.com>
2023-07-28 20:25:30 -0500

    Update: update current user profile response

--- Controllers/UserActions/GET/GetUserProfile.js
+++ Controllers/UserActions/GET/GetUserProfile.js
@@ -41,12 +41,13 @@ exports.GetUserProfile = async (req, res) => {
     if(compareCurrentUserToUserToGet){
       res.json({
         User: "This is the current user",
+        website: userToGetData.website,
         userName: userToGetData.username,
         userBio: userToGetData.bio,
-        profile_picture: userToGetData.profile_picture,
         number_of_posts: userToGetData.number_of_posts,
         number_of_followers: userToGetData.number_of_followers,
         number_of_following: userToGetData.number_of_following,
+        profile_picture: userToGetData.profile_picture,
       });
     }
     else if (userToGetData.privacySettings.private_account) {
@@ -63,6 +64,7 @@ exports.GetUserProfile = async (req, res) => {
       res.json({
         userName: userToGetData.username,
         userBio: userToGetData.bio,
+        website: userToGetData.website,
         profile_picture: userToGetData.profile_picture,
         number_of_posts: userToGetData.number_of_posts,
         number_of_followers: userToGetData.number_of_followers,
