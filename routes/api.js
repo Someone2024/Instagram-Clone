f87633cf5a2b9814efc8d6a41b353a4e36056a77
Someone <bassilsantiago@gmail.com>
2023-07-30 13:14:24 -0500

    Refactor: change every enpoint's name to follow rest conventions

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../Controllers/userController");
-const checkAuh = require("../Controllers/checkAuth");
+const postController = require("../Controllers/userController");
+const commentController = require("../Controllers/userController");
+
+const checkAuth = require("../Controllers/checkAuth");
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: "uploads/",
@@ -9,44 +12,37 @@ const storage = multer.diskStorage({
     cb(null, file.originalname);
   },
 });
-
 const upload = multer({ storage: storage });
 
-//GET Requests
-router.get("/:username", checkAuh, userController.GetUserProfile);
+// GET Requests
+router.get("/users/:username", checkAuth, userController.GetUserProfile);
+// router.get("/users/:username/suggested", checkAuth, userController.GetSuggestedUsers);
+// router.get("/users/:username/interactions/likes", checkAuth, userController.GetLikedPosts);
+// router.get("/users/:username/posts", checkAuth, userController.GetUsersPosts);
+// router.get("/users/:username/posts/:postid", checkAuth, userController.GetPostById);
+// router.get("/explore/:limit", checkAuth, userController.GetInfiniteScrollPosts);
+// router.get("/reels/:reelid", checkAuth, userController.GetReelPost);
+// router.get("/search/users", checkAuth, userController.SearchUsers);
 
-//POST Requests
-router.post("/register", userController.Register);
-router.post("/login", userController.Login);
+// POST Requests
+router.post("/users/register", userController.Register);
+router.post("/users/login", userController.Login);
+router.post("/users/:username/follow", checkAuth, userController.FollowOrUnFollowUser);
+router.post("/users/:username/block", checkAuth, userController.BlockOrUnBlockUser);
+router.post("/users/:postid/like", checkAuth, userController.LikeAndDislikePost);
+router.post("/users/:postid/comment", checkAuth, commentController.CommentPost);
 
-router.post("/:username/follow", checkAuh, userController.FollowOrUnFollowUser);
-router.post("/:username/block", checkAuh, userController.BlockOrUnBlockUser);
-router.post("/:postid/comment", checkAuh, userController.CommentPost);
-router.post(
-  "/upload/:type",
-  upload.single("post"),
-  checkAuh,
-  userController.Upload
-);
+router.post("/users/upload/:type", upload.single("post"), checkAuth, postController.Upload);
 
-//PUT requests
-router.put("/:postid/like", checkAuh, userController.LikePost);
-router.put(
-  "/settings/profile/picture",
-  upload.single("file"),
-  checkAuh,
-  userController.UpdateUserProfilePicture
-);
-router.put(
-  "/settings/profile/privacy",
-  checkAuh,
-  userController.UpdateUserPrivacy
-);
-router.put("/settings/profile", checkAuh, userController.UpdateUserProfile);
+// PUT Requests
+router.put("/users/:username/settings/profile", checkAuth, userController.UpdateUserProfile);
+router.put("/users/:username/settings/profile-picture", checkAuth, userController.UpdateUserProfilePicture);
 
-//DELETE requests
-router.delete("/delete/post/:postid", checkAuh, userController.DeletePost)
-router.delete("/settings/delete-account", checkAuh, userController.DeleteUserAccount)
-router.delete("/delete/comment/:commentid", checkAuh, userController.DeleteComment)
+router.put("/users/:username/settings/privacy", checkAuth, userController.UpdateUserPrivacy);
+
+// DELETE Requests
+router.delete("/users/:username/posts/:postid", checkAuth, postController.DeletePost);
+router.delete("/users/settings/delete-account", checkAuth, userController.DeleteUserAccount);
+router.delete("/users/:postid/delete/comment/:commentid", checkAuth, commentController.DeleteComment);
 
 module.exports = router;
