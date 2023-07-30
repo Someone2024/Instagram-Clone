ba566356e1a61310b68383dd61c588fef4ba8c74
Someone <bassilsantiago@gmail.com>
2023-07-30 12:23:34 -0500

    Style: Organize imports by request type

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,15 +1,20 @@
 const { GetUserProfile } = require("./UserActions/GET/GetUserProfile");
+
 const { Register } = require("./UserActions/POST/RegisterUser");
 const { Login } = require("./UserActions/POST/LoginUser");
 const { FollowOrUnFollowUser } = require("./UserActions/POST/FollowOrUnUser");
 const { BlockOrUnBlockUser } = require("./UserActions/POST/BlockOrUnUser");
 const { Upload } = require("./UserActions/POST/Upload");
+const { CommentPost } = require("./UserActions/POST/CommentPost");
+
 const { UpdateUserPrivacy } = require("./UserActions/PUT/UpdateUserPrivacy");
 const { UpdateUserProfilePicture } = require("./UserActions/PUT/UpdateUserProfilePicture");
 const { UpdateUserProfile } = require("./UserActions/PUT/UpdateUserProfile");
 const { LikePost } = require("./UserActions/PUT/LikePost");
+
 const { DeletePost } = require("./UserActions/DELETE/DeletePost");
 const { DeleteUserAccount } = require("./UserActions/DELETE/DeleteUserAccount");
+const { DeleteComment } = require("./UserActions/DELETE/DeleteComment");
 
 //GET requests
 exports.GetUserProfile = GetUserProfile;
@@ -25,6 +30,8 @@ exports.Register = Register;
 
 exports.Login = Login;
 
+exports.CommentPost = CommentPost;
+
 //PUT requests
 exports.UpdateUserPrivacy = UpdateUserPrivacy;
 
@@ -36,6 +43,8 @@ exports.LikePost = LikePost;
 
 //DELETE requests
 
-exports.DeletePost = DeletePost;
+exports.DeletePost = DeletePost; //Delete comments associated with the post
 
-exports.DeleteUserAccount = DeleteUserAccount;
+exports.DeleteUserAccount = DeleteUserAccount; //Delete comments associated with the user
+
+exports.DeleteComment = DeleteComment
