2a2568f4391ab504ede5ca33b57be2eb1834b176
Someone <bassilsantiago@gmail.com>
2023-07-30 12:38:29 -0500

    Feat: Deletes every comment related to the deleted post

--- Controllers/UserActions/DELETE/DeletePost.js
+++ Controllers/UserActions/DELETE/DeletePost.js
@@ -1,6 +1,6 @@
 const { deleteDoc, increment, updateDoc } = require("firebase/firestore");
 const { getDocs, query, where, limit } = require("firebase/firestore");
-const { PostsRef, UsersRef } = require("../../../FirebaseApp");
+const { PostsRef, UsersRef, CommentsRef } = require("../../../FirebaseApp");
 
 exports.DeletePost = async (req, res) => {
   const PostId = req.params.postid;
@@ -18,14 +18,23 @@ exports.DeletePost = async (req, res) => {
     limit(1)
   );
 
+  const CommentsToDeleteQuery = query(
+    CommentsRef,
+    where("postId", "==", PostId),
+  );
+
   try {
     const currentUser = (await getDocs(currentUserQuery)).docs[0].ref;
     const PostToDelete = (await getDocs(PostToDeleteQuery)).docs[0].ref;
+    const CommentsToDelete = (await getDocs(CommentsToDeleteQuery)).docs;
+    
     await deleteDoc(PostToDelete);
     await updateDoc(currentUser, {
       number_of_posts: increment(-1),
     });
-
+    if(CommentsToDelete) {
+      CommentsToDelete.forEach(async comment => await deleteDoc(comment.ref))
+    }
     res.json({ message: "Post deleted successfully" });
   } catch (error) {
     return res
