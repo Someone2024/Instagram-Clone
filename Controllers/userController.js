1855d84ee116d9adcddd190885a2ae191a9827af
santiago <bassilsantiago@gmail.com>
2023-08-03 11:57:09 -0500

    Import user activuty controllers

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,8 +1,12 @@
 const { GetUserProfile } = require("./UserActions/GET/GetUserProfile");
 const { GetUserSinglePosts } = require("./UserActions/GET/GetUserSinglePosts");
 const { GetSuggestedUsers } = require("./UserActions/GET/GetSuggestedUsers");
-const { GetInfiniteFeedPosts } = require("./UserActions/GET/GetInfiniteFeedPosts");
+const {
+  GetInfiniteFeedPosts,
+} = require("./UserActions/GET/GetInfiniteFeedPosts");
 const { SearchUsers } = require("./UserActions/GET/SearchUsers");
+const { GetUserLikedPosts } = require("./UserActions/GET/GetUserLikedPosts");
+const { GetUserCommentedPosts } = require("./UserActions/GET/GetUserCommentedPosts");
 
 const { Register } = require("./UserActions/POST/RegisterUser");
 const { Login } = require("./UserActions/POST/LoginUser");
@@ -10,7 +14,9 @@ const { FollowOrUnFollowUser } = require("./UserActions/POST/FollowOrUnUser");
 const { BlockOrUnBlockUser } = require("./UserActions/POST/BlockOrUnUser");
 
 const { UpdateUserPrivacy } = require("./UserActions/PUT/UpdateUserPrivacy");
-const { UpdateUserProfilePicture } = require("./UserActions/PUT/UpdateUserProfilePicture");
+const {
+  UpdateUserProfilePicture,
+} = require("./UserActions/PUT/UpdateUserProfilePicture");
 const { UpdateUserProfile } = require("./UserActions/PUT/UpdateUserProfile");
 
 const { DeleteUserAccount } = require("./UserActions/DELETE/DeleteUserAccount");
@@ -20,11 +26,15 @@ exports.GetUserProfile = GetUserProfile;
 
 exports.GetSuggestedUsers = GetSuggestedUsers;
 
-exports.GetUserSinglePosts = GetUserSinglePosts
+exports.GetUserSinglePosts = GetUserSinglePosts;
 
-exports.GetInfiniteFeedPosts = GetInfiniteFeedPosts
+exports.GetInfiniteFeedPosts = GetInfiniteFeedPosts;
 
-exports.SearchUsers = SearchUsers
+exports.SearchUsers = SearchUsers;
+
+exports.GetUserLikedPosts = GetUserLikedPosts;
+
+exports.GetUserCommentedPosts = GetUserCommentedPosts;
 
 //POST requests
 exports.FollowOrUnFollowUser = FollowOrUnFollowUser;
